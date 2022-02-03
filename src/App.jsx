50624db608fe46c04bad0cb410d47c79715a64a2
DevopsLuixus <ing.luismadrid@gmail.com>
2022-02-03 17:08:20 -0500

    sync

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Formulario from './components/Formulario';
+import Header from './components/Header';
+import { ListadoPacientes } from './components/ListadoPacientes';
+
+
+
+
+
+function App() {
+  //codigo javascript
+
+  return (
+    //lo que se va en pantalla
+    <div className='container mx-auto mt-20'>
+      <Header />
+        <div className='mt-12 md:flex'>
+          <Formulario />
+          <ListadoPacientes />
+        </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const ListadoPacientes = () => {
+  return(
+   <div className='w-1/2 lg:w-3/5'>
+      <h2>
+          Listado Pacientes
+      </h2>
+  </div>
+  )
+};
